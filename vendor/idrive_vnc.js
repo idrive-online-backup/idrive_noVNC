8b7f6332c2367d80b5dee4dd2f8f8de67289e97d
pawan-idrive <36025508+pawan-idrive@users.noreply.github.com>
2021-06-16 12:47:10 +0530

    refined the code for clipboard text

--- vendor/idrive_vnc.js
+++ vendor/idrive_vnc.js
@@ -42,58 +42,31 @@ function handleConnect() {
   // Handle Clipboard Paste
   let clipboardButton = document.getElementById('sendClipboardButton');
   clipboardButton.addEventListener("click", function() {
-    if (!window.isSecureContext) {
+    if (!window.isSecureContext || null === myRFB) {
       return;
     }
-    if (null === myRFB) {
-      return;
-    }
-    navigator.permissions.query({name: 'clipboard-read'}).then(result => {
-      if (result.state != 'granted') {
-        if (result.state == 'prompt') {
-            clipboardButton.value = 'Prompted';
-            navigator.clipboard.readText().then(() => {
-              clipboardButton.value = 'Send Clipboard';
-            });
-        }
-        return;
-      }
 
-      navigator.clipboard.readText().then(text => {
-        getPastingText(text);
-      }).catch(err => {
-        clipboardButton.value = 'Send Clipboard';
-      });
+    navigator.permissions.query({name: 'clipboard-read'}).then(result => {
+      if (result.state == "granted" || result.state == "prompt") {
+        navigator.clipboard.readText().then(text => {
+          if (text.length > 0) {
+            sendClipBoardTextToConsole(text);
+          }
+        });
+      }
     });
   });
 
-  function getPastingText(text) {
-    let count = text.length;
-    if (count === 0) {
-      clipboardButton.value = 'Send Clipboard';
-      return;
-    }
-    sendTextToConsole(text);
-
-    // noVNC has a 50ms interval timer when sending keys.
-    setTimeout(function() {
-      clipboardButton.value = 'Send Clipboard';
-    }, count * 50)
-  }
-
-  function sendTextToConsole(text) {
+  function sendClipBoardTextToConsole(text) {
     let char = text[0];
     text = text.slice(1);
     let char_code = char.charCodeAt();
-    let use_shift_key = char.match(/[A-Z!@#$%^&*()_+{}:"<>?~|]/);
-    let enter_key = char == "\n";
 
-    // handler enter_key and shift key chars
-    if (enter_key) {
+    if ("\n" === char) {
       myRFB.sendKey(0xff0d,0xff0d,true);
       myRFB.sendKey(0xff0d,0xff0d,false);
     } else {
-      if (use_shift_key) {
+      if (char.match(/[A-Z!@#$%^&*()_+{}:"<>?~|]/)) {
         myRFB.sendKey(0xffe1,0xffe1,true);
         myRFB.sendKey(char_code,char_code,true);
         myRFB.sendKey(char_code,char_code,false);
@@ -105,7 +78,7 @@ function handleConnect() {
     }
     // check for complete text
     if (text.length > 0) {
-      setTimeout(sendTextToConsole, 50, text);
+      setTimeout(sendClipBoardTextToConsole, 50, text);
       return;
     }
   }
